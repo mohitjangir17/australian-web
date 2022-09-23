53db0ddf5fa3f3146726d3389343ce4f38480c15
Mohit Jangir <mohitjangir17@gmail.com>
2022-09-23 12:37:50 +0530

    hash added

--- src/components/Courses.jsx
+++ src/components/Courses.jsx
@@ -6,37 +6,37 @@ function Courses() {
       <span>Courses</span>
       <h2>Courses that we provide</h2>
       <section className="course-item-container">
-        <a href="">
+        <a href="#">
           <div className="course-item">
             <img width={80} src={logo} alt="course" />
             <h4>Web Development</h4>
           </div>
         </a>
-        <a href="">
+        <a href="#">
           <div className="course-item">
             <img width={80} src={logo} alt="course" />
             <h4>Web Development</h4>
           </div>
         </a>
-        <a href="">
+        <a href="#">
           <div className="course-item">
             <img width={80} src={logo} alt="course" />
             <h4>Web Development</h4>
           </div>
         </a>
-        <a href="">
+        <a href="#">
           <div className="course-item">
             <img width={80} src={logo} alt="course" />
             <h4>Web Development</h4>
           </div>
         </a>
-        <a href="">
+        <a href="#">
           <div className="course-item">
             <img width={80} src={logo} alt="course" />
             <h4>Web Development</h4>
           </div>
         </a>
-        <a href="">
+        <a href="#">
           <div className="course-item">
             <img width={80} src={logo} alt="course" />
             <h4>Web Development</h4>
